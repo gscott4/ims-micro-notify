a978f9203cd0c9f0d164e386b903dd3f9be552d7
grays <grays@LAPTOP-7OJD8FRQ.attlocal.net>
2018-09-06 20:37:26 -0400

    First commit

---
+++ ims-micro-notify/src/main/java/org/jee8ng/ims/notify/JaxrsActivator.java
@@ -0,0 +1,9 @@
+package org.jee8ng.ims.notify;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JaxrsActivator extends Application {
+
+}
